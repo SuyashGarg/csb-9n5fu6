19e4a283496cb1dc89a7bbf2b206ade23de0f4f6
Code Sandbox <hello@codesandbox.io>
2022-12-30 07:15:29 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.0b05620625bdc165aae33b1d330185ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c39157e9f6c8c1e113ad1a610f792af",
+    "url": "https://suyashgarg.github.io/csb-9n5fu6/index.html"
+  },
+  {
+    "revision": "89fe6d31190a4fcf4ac2",
+    "url": "https://suyashgarg.github.io/csb-9n5fu6/static/css/main.a450578b.chunk.css"
+  },
+  {
+    "revision": "2d1a55d4c6d72f6dc93a",
+    "url": "https://suyashgarg.github.io/csb-9n5fu6/static/js/2.606d2c25.chunk.js"
+  },
+  {
+    "revision": "89fe6d31190a4fcf4ac2",
+    "url": "https://suyashgarg.github.io/csb-9n5fu6/static/js/main.fe8d6d95.chunk.js"
+  },
+  {
+    "revision": "c7a36d243285861e7ca4",
+    "url": "https://suyashgarg.github.io/csb-9n5fu6/static/js/runtime~main.e21ea2df.js"
+  }
+]);
